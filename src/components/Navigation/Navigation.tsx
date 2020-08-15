396861496a440aeeea61c943ab3e7d9efac63977
briantran98 <98brian.tran@gmail.com>
2020-08-15 16:37:05 -0700

    Created some basic routing

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Navigation.scss";
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
-  return <p>Brian Tran</p>;
+  return (
+    <div>
+      <ul>
+        <li><Link to="/">Brian Tran</Link></li>
+        <li><Link to="/home">Home</Link></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
