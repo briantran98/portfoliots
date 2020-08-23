1a8f0805f60a8fa4e16077381081f4f5a0fab232
briantran98 <98brian.tran@gmail.com>
2020-08-22 19:23:12 -0700

    Added another test for the root page

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -3,10 +3,13 @@ import { render } from "@testing-library/react";
 
 import App from '../../App'
 
-describe("App", () => {
+describe("Root page renders correctly", () => {
 	it("root page shows 'Intro: Landing page'", () => {
 		const { getByText } = render(<App />);
-
 		expect(getByText("Intro: Landing page")).toBeInTheDocument();
+	})
+	it("List of navigational pages renders", () => {
+		const { getByText } = render(<App />);
+		expect(getByText("Projects")).toBeInTheDocument();
 	})
 });
